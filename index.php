17fc1bbcaa196c27a6a63ceea44a826c9793fc0e
Joel Thoman <jethoman@gmail.com>
2017-02-22 21:40:17 -0500

    W3 Validator error fixes

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 
     <body>
         <h1>Scrabblish Point Counter</h1>
-        <img src="images/letters.jpeg" alt="picture of scrabble letters" width="300px">
+        <img src="images/letters.jpeg" alt="picture of scrabble letters" width="300">
         <?php if(isset($scrabEntry)and (!$errors)): ?>
           <?="<div class='alert-ok'>Your word is worth ".$scrabEntry." points!</div>"?>
         <?php endif; ?>
@@ -26,18 +26,18 @@
                 <input type='text' name='word' value="<?php if(isset($scrab)) echo $scrab ?>">
             </label><br>
             <fieldset class='adjustments'>
-                <legend>Bonus Points<br>
+                <legend>Bonus Points</legend>
                     <label><input type='radio' name='bonus' value='none'
                       <?php if($bonus == 'none' or $bonus == '') echo 'CHECKED'?>> None</label>
                     <label><input type='radio' name='bonus' value='double'
                       <?php if($bonus == 'double') echo 'CHECKED'?>> Double Word Score</label>
                     <label><input type='radio' name='bonus' value='triple'
                       <?php if($bonus == 'triple') echo 'CHECKED'?>> Triple Word Score</label>
-                </legend>
-                <legend>Include 50 point "bingo"? (word must be at least 7 letters)<br>
+
+                <legend>Include 50 point "bingo"? (word must be at least 7 letters)</legend>
                     <label><input type='checkbox' name='bingo' value='yes'
                       <?php if($bingo == 'yes') echo 'CHECKED'?>> Yes</label>
-                </legend>
+
             </fieldset>
             <input type='submit' class='btm btm-primary btn-small'>
 
