6a89d260d889a3f2cf44fdede70976abe7b5f4d3
Joel Thoman <jethoman@gmail.com>
2017-02-22 21:16:58 -0500

    bug fixes

--- index.php
+++ index.php
@@ -5,44 +5,42 @@
     <head>
     <title>Scrabblish Point Counter</title>
     <meta charset='utf-8'>
-    <!--<link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' rel='stylesheet'>-->
-    <!--<link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/flatly/bootstrap.min.css' rel='stylesheet'>-->
     <link href='css/style.css' rel='stylesheet'>
     </head>
 
-<body>
-    <h1>Scrabblish Point Counter</h1>
-    <img src="images/letters.jpeg" alt="picture of scrabble letters" width="300px">
-    <form method='GET' action='index.php'>
-        <label>Enter the word you played:
-            <input type='text' name='word' value="<?php if(isset($scrab)) echo $scrab ?>">
-        </label><br>
-        <fieldset class='adjustments'>
+    <body>
+        <h1>Scrabblish Point Counter</h1>
+        <img src="images/letters.jpeg" alt="picture of scrabble letters" width="300px">
+        <form method='GET' action='index.php'>
+            <label>Enter the word you played:
+                <input type='text' name='word' value="<?php if(isset($scrab)) echo $scrab ?>">
+            </label><br>
+            <fieldset class='adjustments'>
                 <legend>Bonus Points<br>
-                <label><input type='radio' name='bonus' value='none'
-                  <?php if($bonus == 'none' or $bonus == '') echo 'CHECKED'?>> None</label>
-                <label><input type='radio' name='bonus' value='double'
-                  <?php if($bonus == 'double') echo 'CHECKED'?>> Double Word Score</label>
-                <label><input type='radio' name='bonus' value='triple'
-                  <?php if($bonus == 'triple') echo 'CHECKED'?>> Triple Word Score</label>
+                    <label><input type='radio' name='bonus' value='none'
+                      <?php if($bonus == 'none' or $bonus == '') echo 'CHECKED'?>> None</label>
+                    <label><input type='radio' name='bonus' value='double'
+                      <?php if($bonus == 'double') echo 'CHECKED'?>> Double Word Score</label>
+                    <label><input type='radio' name='bonus' value='triple'
+                      <?php if($bonus == 'triple') echo 'CHECKED'?>> Triple Word Score</label>
                 </legend>
                 <legend>Include 50 point "bingo"?<br>
-                <label><input type='checkbox' name='bingo' value='yes'
-                  <?php if($bingo == 'yes') echo 'CHECKED'?>> Yes</label>
-                  </legend>
-        </fieldset>
-  <input type='submit' class='btm btm-primary btn-small'>
-  <?php if($errors): ?>
-    <div class='alert-warning'>
-      <?php foreach($errors as $error): ?>
-        <?=$error?><br>
-      <?php endforeach; ?>
-  </div>
-<?php endif; ?>
-</form>
-<?php if(isset($scrabEntry)and (!$errors)): ?>
-  <?="<div class='alert-ok'>Your score is ".$scrabEntry."</div>"?>
-<?php endif; ?>
+                    <label><input type='checkbox' name='bingo' value='yes'
+                      <?php if($bingo == 'yes') echo 'CHECKED'?>> Yes</label>
+                </legend>
+            </fieldset>
+            <input type='submit' class='btm btm-primary btn-small'>
+              <?php if($errors): ?>
+                <div class='alert-warning'>
+                  <?php foreach($errors as $error): ?>
+                    <?=$error?><br>
+                  <?php endforeach; ?>
+              </div>
+            <?php endif; ?>
+        </form>
+        <?php if(isset($scrabEntry)and (!$errors)): ?>
+          <?="<div class='alert-ok'>Your score is ".$scrabEntry."</div>"?>
+        <?php endif; ?>
 
-</body>
+    </body>
 </html>
